5431c89308472bb94213e3408a47a5d32215549f
Igor Lazarev <lazarigo@fel.cvut.cz>
2022-05-18 02:37:41 +0200

    Added objects

---
+++ src/object/OBJ_Key.java
@@ -0,0 +1,18 @@
+package object;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.util.Objects;
+
+public class OBJ_Key extends SuperObject {
+
+    public OBJ_Key() {
+        name = "Key";
+
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/objects/key.png")));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
